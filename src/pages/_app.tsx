d379f25a17ea5390a83792e01064f9659c05b758
Bori-github <qhflrnfl4324@gmail.com>
2023-02-14 14:47:31 +0900

    💄 #1 - Global font 적용
    - Pretendard  localFont, Montserrat googleFont 적용
    - theme을 이용하여 global font style 적용
    - theme 타입 선언(emotion.d.ts)
    - nextConfig fontLoaders 설정 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import { Global } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import { global } from 'styles/globals';
+import theme from 'styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Global styles={global} />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,113 @@
+import { css } from '@emotion/react';
+import { Montserrat } from '@next/font/google';
+import localFont from '@next/font/local';
+import type { SerializedStyles, Theme } from '@emotion/react';
+import type { NextFont } from '@next/font/dist/types';
+
+const pretendard = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  display: 'block',
+  fallback: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+});
+
+const montserrat = Montserrat({
+  weight: '700',
+  style: 'normal',
+  display: 'block',
+  fallback: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+  subsets: ['latin'],
+});
+
+interface Font {
+  fontFamily: string | NextFont;
+  size: number;
+  weight: 400 | 500 | 700;
+  lineHeight: number;
+  letterSpacing?: number;
+}
+
+const fontStyle = ({
+  fontFamily,
+  size,
+  weight,
+  lineHeight,
+  letterSpacing,
+}: Font): SerializedStyles => {
+  return css`
+    font-family: ${fontFamily};
+    font-size: ${size}px;
+    font-weight: ${weight};
+    line-height: ${lineHeight}%;
+    letter-spacing: ${letterSpacing}em;
+  `;
+};
+
+const theme: Theme = {
+  fonts: {
+    diary_title: fontStyle({
+      fontFamily: montserrat.style.fontFamily,
+      size: 20,
+      weight: 700,
+      lineHeight: 140,
+    }),
+    diary_body: fontStyle({
+      fontFamily: pretendard.style.fontFamily,
+      size: 14,
+      weight: 400,
+      lineHeight: 160,
+    }),
+    diary_info: fontStyle({
+      fontFamily: pretendard.style.fontFamily,
+      size: 12,
+      weight: 400,
+      lineHeight: 100,
+      letterSpacing: -0.02,
+    }),
+  },
+};
+
+export default theme;
