abf040a6534bd3927c8c5a4315412774288e4db6
Bori-github <qhflrnfl4324@gmail.com>
2023-02-15 15:35:37 +0900

    💄 #8 - Color palette 컬러값 상수로 지정

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+const colors = Object.freeze({
+  white: '#FFFFFF',
+  gray_eee: '#EEEEEE',
+  gray_ddd: '#DDDDDD',
+  gray_ccc: '#CCCCCC',
+  gray_bbb: '#BBBBBB',
+  gray_999: '#999999',
+  gray_666: '#666666',
+  black: '#222222',
+  bg_f5fdf7: '#F5FDF7',
+  bg_fafafa: '#FAFAFA',
+  bg_f4f4f4: '#F4F4F4',
+  main: '#00C73C',
+  error: '#F84D4D',
+  pink: '#FF89D7',
+});
+
+export default colors;
