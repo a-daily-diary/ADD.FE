538c9bdaeffd50be3f6ecf486090e47a28d354be
Bori-github <qhflrnfl4324@gmail.com>
2023-02-24 00:33:46 +0900

    💄 #30 - defalut button style reset 추가
    - 브라우저 기본 버튼 스타일을 없애기 위해 reset 스타일 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -19,6 +19,35 @@ const GlobalStyle = css`
     text-decoration: none;
   }
 
+  button {
+    overflow: visible;
+    width: auto;
+    margin: 0;
+    padding: 0;
+    border: none;
+    background: transparent;
+    color: inherit;
+    font: inherit;
+    text-align: inherit;
+    cursor: pointer;
+
+    /* Normalize 'line-height'. Cannot be changed from 'normal' in Firefox 4+. */
+    line-height: normal;
+
+    /* Corrects font smoothing for webkit */
+    -webkit-font-smoothing: inherit;
+    -moz-osx-font-smoothing: inherit;
+
+    /* Corrects inability to style clickable 'input' types in iOS */
+    -webkit-appearance: none;
+
+    /* Remove excess padding and border in Firefox 4+ */
+    &::-moz-focus-inner {
+      border: 0;
+      padding: 0;
+    }
+  }
+
   @media (prefers-color-scheme: dark) {
     html {
       color-scheme: dark;
