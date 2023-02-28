93f46138d02396096af493cb724387b268a2f833
Bori-github <qhflrnfl4324@gmail.com>
2023-02-28 17:05:38 +0900

    🔥 #23 - 메인 페이지의 불필요한 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled';
 import Head from 'next/head';
 import type { NextPage } from 'next';
 import DiaryList from 'components/diary/DiaryList';
@@ -10,15 +9,9 @@ const Home: NextPage = () => {
         <title>Home | ADD</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Text>ADD Front End</Text>
       <DiaryList />
     </>
   );
 };
 
 export default Home;
-
-const Text = styled.h1`
-  ${({ theme }) => theme.fonts.diary_title};
-  color: ${({ theme }) => theme.colors.main};
-`;
