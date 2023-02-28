78cf6d68746c1fee445313cf1797c3c4c13fd8b3
Bori-github <qhflrnfl4324@gmail.com>
2023-02-28 15:22:15 +0900

    💄 #23 - ellisis style 생성
    - 텓스트가 4줄이 넘어가는 경우 ...으로 표시

---
+++ src/styles/EllipsisStyle.ts
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react';
+
+export const EllipsisStyle = css`
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+`;
