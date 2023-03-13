a9ad78d17e1f6d60aea35ec66ec1348020fabd99
Bori-github <qhflrnfl4324@gmail.com>
2023-03-13 15:31:24 +0900

    🥅 #44 - ISO 형식의 날짜 string이 아닐경우 발생하는 에러 처리
    - 날짜 포맷터 함수인 dateFormat, timeFormat 내에서 에러 발생
    - 유효하지 않은 string이 넘어간 경우 NaN을 반환하는 Date.parse를 이용하여 에러 처리

--- src/utils/Formatter.ts
+++ src/utils/Formatter.ts
@@ -1,30 +1,32 @@
-export const dateFormat = (date: Date): string => {
+export const dateFormat = (dateString: string): string | null => {
+  // invalid date 에러 방어 코드
+  if (isNaN(Date.parse(dateString))) return null;
+
+  const date = new Date(dateString);
   const convertDate = new Intl.DateTimeFormat('ko-KR', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
   })
     .format(date)
-    .replace(/ /g, '');
+    .slice(0, -1);
 
-  return convertDate.slice(0, -1);
+  return convertDate;
   // 예: 2020.08.29
 };
 
-export const timeFormat = (date: Date): string | null => {
-  const nowDate = new Date();
-  const createdDate = new Date(date);
-  const minTimeGap =
-    Math.floor(nowDate.getTime() - createdDate.getTime()) / 1000 / 60;
+export const timeFormat = (dateString: string): string | null => {
+  // invalid date 에러 방어 코드
+  if (isNaN(Date.parse(dateString))) return null;
 
-  if (minTimeGap < 1) {
-    return '방금 전';
-  }
-  if (minTimeGap < 60) {
-    return `${Math.floor(minTimeGap)}분 전`;
-  }
-  if (minTimeGap / 24 < 24) {
-    return `${Math.floor(minTimeGap / 24)}시간 전`;
-  }
+  const nowDate = new Date();
+  const date = new Date(dateString);
+  const minTimeGap = Math.floor(nowDate.getTime() - date.getTime()) / 1000 / 60;
+
+  if (minTimeGap < 0) return null;
+  if (minTimeGap < 1) return '방금 전';
+  if (minTimeGap < 60) return `${Math.floor(minTimeGap)}분 전`;
+  if (minTimeGap / 24 < 24) return `${Math.floor(minTimeGap / 24)}시간 전`;
+
   return null;
 };
