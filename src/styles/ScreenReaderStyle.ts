d7f3a999997b299da4d10c0d05ef4e344d8c1103
Bori-github <qhflrnfl4324@gmail.com>
2023-03-19 20:41:31 +0900

    💄 close #50 - sr-only, not-sr-only 스타일 추가

---
+++ src/styles/ScreenReaderStyle.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+
+export const ScreenReaderOnly = css`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
+`;
+
+export const NotScreenReaderOnly = css`
+  position: static;
+  width: auto;
+  height: auto;
+  padding: 0;
+  margin: 0;
+  overflow: visible;
+  clip: auto;
+  white-space: normal;
+`;
