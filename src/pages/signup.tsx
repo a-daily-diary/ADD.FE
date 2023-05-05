dff865657e343d215ab5c744b041b93a72d757f2
choisung2 <c0817d@gmail.com>
2023-05-05 17:35:27 +0900

    💄 회원가입 UI 구현

---
+++ src/pages/signup.tsx
@@ -0,0 +1,81 @@
+import styled from '@emotion/styled';
+
+import { useState } from 'react';
+
+import Seo from 'components/common/Seo';
+
+import Email from 'components/signup/Email';
+import Profile from 'components/signup/Profile';
+import Terms from 'components/signup/Terms';
+
+interface IEnterForm {
+  email: string;
+  username: string;
+  password: string;
+  passwordCheck: string;
+  image: string;
+  isAgree: boolean;
+}
+
+const SignUp = () => {
+  const [formData, setFormData] = useState<IEnterForm>({
+    email: '',
+    username: '',
+    password: '',
+    passwordCheck: '',
+    image: '',
+    isAgree: false,
+  });
+
+  return (
+    <>
+      <Seo title="회원가입 | a daily diary" />
+      <main>
+        <FormWrapper>
+          {formData.email.length === 0 && (
+            <>
+              <Title>이메일을 입력해주세요.</Title>
+              <Email formData={formData} setFormData={setFormData} />
+            </>
+          )}
+          {formData.email.length > 0 && formData.image === '' && (
+            <>
+              <Title>프로필 사진을 등록해주세요.</Title>
+              <Description>
+                프로필로 등록할 사진을 앨범에서 가져오시거나, <br /> 기본 프로필
+                이미지에서 선택해주세요.
+              </Description>
+              <Profile formData={formData} setFormData={setFormData} />
+            </>
+          )}
+          {formData.image.length > 0 && !formData.isAgree && (
+            <>
+              <Title>약관에 동의해주세요.</Title>
+              <Terms formData={formData} setFormData={setFormData} />
+            </>
+          )}
+          {formData.isAgree && <p>회원가입 완료</p>}
+        </FormWrapper>
+      </main>
+    </>
+  );
+};
+
+export default SignUp;
+
+const Title = styled.h1`
+  ${({ theme }) => theme.fonts.headline_01}
+`;
+
+const Description = styled.p`
+  margin: 8px 0 0 0;
+  ${({ theme }) => theme.fonts.body_07}
+  color: ${({ theme }) => theme.colors.gray_02};
+`;
+
+const FormWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding: 28px 20px 12px;
+`;
