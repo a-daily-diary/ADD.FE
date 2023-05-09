64412bfec6aff19de6381613326deb7326ef9ca9
Bori-github <qhflrnfl4324@gmail.com>
2023-05-09 14:48:21 +0900

    💄 #69 - RegisterFrom 스타일 코드 수정
    - RegisterFrom 스타일 코드 수정을 위해 register 스타일 코드도 수정

--- src/pages/account/register.tsx
+++ src/pages/account/register.tsx
@@ -19,38 +19,37 @@ const Register = () => {
   return (
     <>
       <Seo title="회원가입 | a daily diary" />
-      <main>
-        <FormWrapper>
-          {formData.email.length === 0 && (
-            <>
-              <Title>이메일을 입력해주세요.</Title>
-              <RegisterForm formData={formData} setFormData={setFormData} />
-            </>
-          )}
-          {formData.email.length > 0 && formData.imgUrl === '' && (
-            <>
-              <Title>프로필 사진을 등록해주세요.</Title>
-              <Description>
-                프로필로 등록할 사진을 앨범에서 가져오시거나, <br /> 기본 프로필
-                이미지에서 선택해주세요.
-              </Description>
-              <Profile formData={formData} setFormData={setFormData} />
-            </>
-          )}
-          {formData.imgUrl.length > 0 && !formData.isAgree && (
-            <>
-              <Title>약관에 동의해주세요.</Title>
-              <Terms formData={formData} setFormData={setFormData} />
-            </>
-          )}
-          {formData.isAgree && <p>회원가입 완료</p>}
-        </FormWrapper>
-      </main>
+      <Container>
+        {formData.email.length === 0 && (
+          <RegisterForm formData={formData} setFormData={setFormData} />
+        )}
+        {formData.email.length > 0 && formData.imgUrl === '' && (
+          <>
+            <Title>프로필 사진을 등록해주세요.</Title>
+            <Description>
+              프로필로 등록할 사진을 앨범에서 가져오시거나, <br /> 기본 프로필
+              이미지에서 선택해주세요.
+            </Description>
+            <Profile formData={formData} setFormData={setFormData} />
+          </>
+        )}
+        {formData.imgUrl.length > 0 && !formData.isAgree && (
+          <>
+            <Title>약관에 동의해주세요.</Title>
+            <Terms formData={formData} setFormData={setFormData} />
+          </>
+        )}
+        {formData.isAgree && <p>회원가입 완료</p>}
+      </Container>
     </>
   );
 };
 
 export default Register;
+
+const Container = styled.div`
+  padding: 28px 20px;
+`;
 
 const Title = styled.h1`
   ${({ theme }) => theme.fonts.headline_01}
@@ -60,11 +59,4 @@ const Description = styled.p`
   margin: 8px 0 0 0;
   ${({ theme }) => theme.fonts.body_07}
   color: ${({ theme }) => theme.colors.gray_02};
-`;
-
-const FormWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  padding: 28px 20px 12px;
 `;
