d0153e09f1954f280c7731935eb70e7ad2d73a58
Bori-github <qhflrnfl4324@gmail.com>
2023-05-13 12:46:55 +0900

    ⚡️ #69 - 변경된 RegisterStep에 따른 회원가입 단계 별 컴포넌트 순서 적용

--- src/pages/account/register.tsx
+++ src/pages/account/register.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import type { SubmitHandler, FieldValues } from 'react-hook-form';
-import type { RegisterSchema, RegisterStep } from 'types/Register';
+import type { RegisterStep } from 'types/Register';
 import Profile from 'components/account/Profile';
 import RegisterForm from 'components/account/RegisterForm';
 import Terms from 'components/account/Terms';
@@ -12,10 +12,32 @@ const Register = () => {
   const methods = useForm({ mode: 'onChange' });
   const { handleSubmit } = methods;
 
-  const [registerStep, setRegisterStep] = useState<RegisterStep>('email');
+  const [registerStep, setRegisterStep] = useState<RegisterStep>({
+    email: true,
+    username: false,
+    password: false,
+    passwordCheck: false,
+    imgUrl: false,
+    isAgree: false,
+  });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    if (registerStep === 'email') setRegisterStep('username');
+    if (registerStep.email)
+      setRegisterStep((state) => {
+        return { ...state, username: true };
+      });
+    if (registerStep.username)
+      setRegisterStep((state) => {
+        return { ...state, password: true };
+      });
+    if (registerStep.password)
+      setRegisterStep((state) => {
+        return { ...state, passwordCheck: true };
+      });
+    if (registerStep.passwordCheck)
+      setRegisterStep((state) => {
+        return { ...state, imgUrl: true };
+      });
   };
 
   return (
@@ -23,26 +45,26 @@ const Register = () => {
       <Seo title="회원가입 | a daily diary" />
       <FormProvider {...methods}>
         <From onSubmit={handleSubmit(onSubmit)}>
-          {registerStep !== 'imgUrl' && registerStep !== 'isAgree' && (
+          {!registerStep.imgUrl && !registerStep.isAgree && (
             <RegisterForm registerStep={registerStep} />
           )}
-          {/* {registerStep === 'imgUrl' && (
+          {registerStep.imgUrl && (
             <>
               <Title>프로필 사진을 등록해주세요.</Title>
               <Description>
                 프로필로 등록할 사진을 앨범에서 가져오시거나, <br /> 기본 프로필
                 이미지에서 선택해주세요.
               </Description>
-              <Profile formData={formData} setFormData={setFormData} />
+              {/* <Profile formData={formData} setFormData={setFormData} /> */}
             </>
           )}
-          {registerStep === 'isAgree' && (
+          {registerStep.isAgree && (
             <>
               <Title>약관에 동의해주세요.</Title>
-              <Terms formData={formData} setFormData={setFormData} />
+              {/* <Terms formData={formData} setFormData={setFormData} /> */}
             </>
           )}
-          {formData.isAgree && <p>회원가입 완료</p>} */}
+          {/* {formData.isAgree && <p>회원가입 완료</p>} */}
         </From>
       </FormProvider>
     </>
