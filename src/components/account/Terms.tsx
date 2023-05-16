db1d0a5f4e4c3f0649c6d5f57ea648b7f38a3966
Bori-github <qhflrnfl4324@gmail.com>
2023-05-16 22:31:56 +0900

    ♻️ #83 - 약관동의 체크 기능 리팩토링

--- src/components/account/Terms.tsx
+++ src/components/account/Terms.tsx
@@ -1,91 +1,121 @@
 import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
-import { useForm, useFormContext } from 'react-hook-form';
-import type { ChangeEvent } from 'react';
+import { useFormContext } from 'react-hook-form';
+import type { ChangeEventHandler } from 'react';
 import type { RegisterSchema } from 'types/Register';
+import CheckedOffIcon from 'assets/icons/checkbox_off.svg';
+import CheckedOnIcon from 'assets/icons/checkbox_on.svg';
 
-// essential 이용해서 데이터 넘길 때 필수값, 선택값 나누려고 했음
-const TERMS_AND_CONDITIONS_LIST = [
-  { id: '1', title: '서비스 이용약관', essential: true },
-  { id: '2', title: '개인정보 처리방침', essential: true },
-  { id: '3', title: '제3자 개인정보 활용 동의', essential: true },
-  { id: '4', title: '마케팅 활용 동의', essential: false },
+// 이용 약관 동의 목록
+// 소셜 로그인/회원가입 적용 시 제 3자 개인정보 활용 동의 필요
+const TERMS_AND_CONDITIONS = [
+  { id: 'service', title: '서비스 이용약관', required: true },
+  { id: 'privacy', title: '개인정보 수집 및 이용 동의', required: true },
+  { id: 'marketing', title: '마케팅 활용 동의', required: false },
 ];
+
+interface TermsAgreement {
+  all: boolean;
+  service: boolean;
+  privacy: boolean;
+  marketing: boolean;
+  [key: string]: boolean;
+}
 
 const Terms = () => {
   const { setValue } = useFormContext<RegisterSchema>();
-  const { register } = useForm();
 
-  const [allFlag, setAllFlag] = useState<boolean>(false);
-  const [termsFlag, setTermsFlag] = useState<boolean[]>([
-    false,
-    false,
-    false,
-    false,
-  ]);
-
-  const toggleCheck = (_e: ChangeEvent<HTMLInputElement>, index: number) => {
-    setTermsFlag((prev) => {
-      const arr = { ...prev };
-      arr[index] = !prev[index];
-      return arr;
-    });
-  };
-  const selectAll = (e: ChangeEvent<HTMLInputElement>) => {
-    setAllFlag(e.target.checked);
-    setTermsFlag((prev) => {
-      Object.keys(prev).map((_item, index) => (prev[index] = e.target.checked));
-      return {
-        ...prev,
-      };
-    });
-  };
+  const [agreedToTerms, setAgreedToTerms] = useState<TermsAgreement>({
+    all: false,
+    service: false,
+    privacy: false,
+    marketing: false,
+  });
 
   useEffect(() => {
-    let allChecked = false;
-    if (Object.values(termsFlag).every((item) => item)) {
-      allChecked = true;
+    if (
+      agreedToTerms.service &&
+      agreedToTerms.privacy &&
+      agreedToTerms.marketing
+    ) {
+      setAgreedToTerms((state) => {
+        return { ...state, all: true };
+      });
+    } else {
+      setAgreedToTerms((state) => {
+        return { ...state, all: false };
+      });
     }
-    setAllFlag(allChecked);
-  }, [termsFlag]);
+  }, [agreedToTerms.service, agreedToTerms.privacy, agreedToTerms.marketing]);
 
-  const isAgree = termsFlag[0] && termsFlag[1] && termsFlag[2];
+  const handleOnToggleCheckbox: ChangeEventHandler = (e) => {
+    const { id } = e.target as HTMLInputElement;
+    if (id === 'all') {
+      if (agreedToTerms.all) {
+        setAgreedToTerms({
+          all: false,
+          service: false,
+          privacy: false,
+          marketing: false,
+        });
+      } else {
+        setAgreedToTerms({
+          all: true,
+          service: true,
+          privacy: true,
+          marketing: true,
+        });
+      }
+    }
+    if (id === 'service') {
+      setAgreedToTerms((state) => {
+        return { ...state, service: !state.service };
+      });
+    }
+    if (id === 'privacy') {
+      setAgreedToTerms((state) => {
+        return { ...state, privacy: !state.privacy };
+      });
+    }
+    if (id === 'marketing') {
+      setAgreedToTerms((state) => {
+        return { ...state, marketing: !state.marketing };
+      });
+    }
+  };
 
   return (
     <section>
       <TitleContainer>
         <Title>약관에 동의해주세요.</Title>
       </TitleContainer>
-      <TermsAndConditionsContainer>
-        <TermsAllCheck>
-          <CheckInput
-            id="allTerms"
-            type="checkbox"
-            checked={allFlag}
-            {...register('all')}
-            onChange={selectAll}
-          />
-          <Checklabel htmlFor="allTerms">약관 전체 동의하기</Checklabel>
-        </TermsAllCheck>
-        {TERMS_AND_CONDITIONS_LIST.map((item, index) => {
-          return (
-            <TermsCheck key={index}>
-              <CheckInput
-                id={item.title}
+      <CheckboxLabel htmlFor="all">
+        {agreedToTerms.all ? <CheckedOnIcon /> : <CheckedOffIcon />}
+        <CheckboxInput
+          id="all"
+          type="checkbox"
+          checked={agreedToTerms.all}
+          onChange={handleOnToggleCheckbox}
+        />
+        약관 전체 동의하기
+      </CheckboxLabel>
+      {TERMS_AND_CONDITIONS.map((term) => {
+        const { id, title, required } = term;
+        return (
+          <div key={`terms-and-conditions-${id}`}>
+            <CheckboxLabel htmlFor={id}>
+              {agreedToTerms[id] ? <CheckedOnIcon /> : <CheckedOffIcon />}
+              <CheckboxInput
+                id={id}
                 type="checkbox"
-                checked={termsFlag[index]}
-                {...register(item.title)}
-                onChange={(e) => {
-                  toggleCheck(e, index);
-                }}
+                checked={agreedToTerms[id]}
+                onChange={handleOnToggleCheckbox}
               />
-              <Checklabel htmlFor={item.title}>
-                {item.title} {item.essential ? '(필수)' : '(선택)'}
-              </Checklabel>
-            </TermsCheck>
-          );
-        })}
-      </TermsAndConditionsContainer>
+              {title} {required ? '(필수)' : '(선택)'}
+            </CheckboxLabel>
+          </div>
+        );
+      })}
     </section>
   );
 };
@@ -100,47 +130,14 @@ const Title = styled.h1`
   ${({ theme }) => theme.fonts.headline_01}
 `;
 
-const TermsAndConditionsContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-
-  input:checked + label::before {
-    background: url('images/signup/checkbox_on.png');
-  }
-`;
-
-const TermsAllCheck = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 16px 0;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.gray_06};
-
-  label {
-    ${({ theme }) => theme.fonts.headline_04}
-  }
-`;
-
-const TermsCheck = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const CheckInput = styled.input`
+const CheckboxInput = styled.input`
   display: none;
 `;
 
-const Checklabel = styled.label`
+const CheckboxLabel = styled.label`
   display: flex;
   align-items: center;
+  gap: 10px;
   ${({ theme }) => theme.fonts.body_05}
-
-  ::before {
-    content: '';
-    display: block;
-    width: 20px;
-    height: 20px;
-    margin: 0 10px 0 0;
-    background: url('images/signup/checkbox_off.png');
-  }
+  cursor: pointer;
 `;
