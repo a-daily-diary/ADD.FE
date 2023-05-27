efdc59b335310879ca8b5f8f15df09a4cd0ba045
Bori-github <qhflrnfl4324@gmail.com>
2023-05-27 21:03:58 +0900

    ✨ #96 - userSession을 사용하기 위한 SessionProvider 적용
    - SessionProvider 적용
    - session 타입 정의

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,31 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import Head from 'next/head';
+import { SessionProvider } from 'next-auth/react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import type { Session } from 'next-auth';
 import type { ReactElement, ReactNode } from 'react';
 import { theme, GlobalStyle } from 'styles';
 
-export type NextPageWithLayout = NextPage & {
+export type NextPageWithLayout<P = Record<string, unknown>> = NextPage<P> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 
-type AppPropsWithLayout = AppProps & {
+type AppPropsWithLayout<P> = AppProps<P> & {
   Component: NextPageWithLayout;
+  pageProps: P & {
+    session?: Session;
+  };
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({
+  Component,
+  pageProps,
+}: AppPropsWithLayout<{ session: Session }>) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
@@ -25,6 +33,6 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
         <Global styles={GlobalStyle} />
         {getLayout(<Component {...pageProps} />)}
       </ThemeProvider>
-    </>
+    </SessionProvider>
   );
 }
