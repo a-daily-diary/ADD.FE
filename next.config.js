97d6f55605524e499ce1ccaa59a8fe0dcc0f774f
Bori-github <qhflrnfl4324@gmail.com>
2023-06-04 23:51:09 +0900

    🔧 #103 - Emotion 컴포넌트를 일반 CSS 선택자로 사용하기 위한 next.config 설정

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
+  images: { // NOTE: images domains 설정이 최상위에 있어야 합니다.
     domains: [
       "add.bucket.s3.amazonaws.com",
       "dummyimage.com", // 목데이터의 이미지 URL 도메인, 추후 삭제
       "robohash.org" // 목데이터의 이미지 URL 도메인, 추후 삭제
     ]
   },
-  reactStrictMode: true,
+  compiler: {
+    emotion: true
+  },
   experimental: {
     fontLoaders: [
       { loader: '@next/font/google', options: { subsets: ['latin'] } },
     ],
   },
+  reactStrictMode: true,
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
