c40a2ef84d30e5b668e369fee96eb4b0b7cdd166
Bori-github <qhflrnfl4324@gmail.com>
2023-06-07 17:45:26 +0900

    🏷️ #102 - 일기 작성을 위한 Diary 타입 추가

--- src/types/Diary.ts
+++ src/types/Diary.ts
@@ -1,6 +1,43 @@
+import type { User } from 'next-auth';
+
 export interface DiaryForm {
   title: string;
   content: string;
   imgUrl?: string;
   isPublic: boolean;
+}
+
+/*
+ * Request Data Types
+ */
+
+// 다이어리 작성
+export type DiaryRequest = DiaryForm;
+
+/*
+ * Response Data Types
+ */
+
+// 다이어리 작성
+export interface DiaryResponse {
+  diary: DiaryDetail;
+  // TODO: badge 추가 예정
+}
+
+/*
+ * Other Types
+ */
+
+// 일기 상세
+export interface DiaryDetail {
+  id: string;
+  title: string;
+  content: string;
+  imgUrl?: string;
+  isPublic: boolean;
+  favoriteCount: number;
+  commentCount: number;
+  createdAt: string;
+  updatedAt: string;
+  author: User;
 }
