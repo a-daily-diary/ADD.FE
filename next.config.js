a5fe98bc0fbc46518019fbde9ff393f2d0a6ddbf
Bori-github <qhflrnfl4324@gmail.com>
2023-06-08 00:20:33 +0900

    🔧 #111 - 이미지 포맷 설정

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
       "add.bucket.s3.amazonaws.com",
       "dummyimage.com", // 목데이터의 이미지 URL 도메인, 추후 삭제
       "robohash.org" // 목데이터의 이미지 URL 도메인, 추후 삭제
-    ]
+    ],
+    formats: ['image/avif', 'image/webp'],
   },
   compiler: {
     emotion: true
