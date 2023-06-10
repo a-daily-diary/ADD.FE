c0db4ecf07cc3633403eee108778f0f7f555fec7
Bori-github <qhflrnfl4324@gmail.com>
2023-06-10 16:03:22 +0900

    ✨ #114 - 일기 이미지 업로드 api 구현

--- src/api/image.ts
+++ src/api/image.ts
@@ -1,11 +1,24 @@
-import type { UploadImageRequest, UploadImageResponse } from 'types/Register';
 import type { SuccessResponse } from 'types/Response';
+import type {
+  UploadImageRequest,
+  UploadImageResponse,
+} from 'types/UploadImage';
 import { API_PATH } from 'constants/api/path';
 import axios from 'lib/axios';
 
 export const uploadUserImage = async (imageFormData: UploadImageRequest) => {
   return await axios.post<SuccessResponse<UploadImageResponse>>(
     API_PATH.users.image,
+    imageFormData,
+    {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    },
+  );
+};
+
+export const uploadDiaryImage = async (imageFormData: UploadImageRequest) => {
+  return await axios.post<SuccessResponse<UploadImageResponse>>(
+    API_PATH.diaries.image,
     imageFormData,
     {
       headers: { 'Content-Type': 'multipart/form-data' },
