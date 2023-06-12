06bd402c52a42d28b99133987670e609d3372eb4
Bori-github <qhflrnfl4324@gmail.com>
2023-06-12 14:07:12 +0900

    💄 #117 - 일기 상세 페이지 Header 적용

--- src/pages/diary/[id].tsx
+++ src/pages/diary/[id].tsx
@@ -1,13 +1,34 @@
+import styled from '@emotion/styled';
+import type { ReactElement } from 'react';
+import Seo from 'components/common/Seo';
+import { Layout, Header, HeaderLeft, HeaderRight } from 'components/layouts';
 import DiaryCommentsContainer from 'containers/diary/DiaryCommentsContainer';
 import DiaryContainer from 'containers/diary/DiaryContainer';
 
 const DiaryDetailPage = () => {
   return (
-    <>
+    <Section>
       <DiaryContainer />
       <DiaryCommentsContainer />
-    </>
+    </Section>
+  );
+};
+
+DiaryDetailPage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout>
+      <Seo title={'a daily diary'} />
+      <Header>
+        <HeaderLeft type="이전" />
+        <HeaderRight type="더보기" />
+      </Header>
+      {page}
+    </Layout>
   );
 };
 
 export default DiaryDetailPage;
+
+const Section = styled.section`
+  margin-top: 54px;
+`;
