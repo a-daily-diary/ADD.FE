e5da2e537e322304f8be23e42172e71a3d80ddae
Bori-github <qhflrnfl4324@gmail.com>
2023-06-16 14:33:19 +0900

    ✨ #119 - 일기 삭제 api 연동

--- src/pages/diary/[id].tsx
+++ src/pages/diary/[id].tsx
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled';
+import { isAxiosError } from 'axios';
 import { useRouter } from 'next/router';
 import { type ReactElement } from 'react';
 import type { NextPageWithLayout } from 'pages/_app';
+import type { ErrorResponse } from 'types/Response';
+import * as api from 'api';
 import { EditIcon, TrashIcon } from 'assets/icons';
 import FloatingMenu from 'components/common/FloatingMenu';
 import Seo from 'components/common/Seo';
@@ -9,10 +12,27 @@ import { Layout, Header, HeaderLeft, HeaderRight } from 'components/layouts';
 import DiaryCommentsContainer from 'containers/diary/DiaryCommentsContainer';
 import DiaryContainer from 'containers/diary/DiaryContainer';
 import useClickOutside from 'hooks/useClickOutside';
+import { errorResponseMessage } from 'utils';
 
 const DiaryDetailPage: NextPageWithLayout = () => {
   const router = useRouter();
+  const { id } = router.query;
   const { ref, isVisible, setIsVisible } = useClickOutside();
+
+  const handleDeleteDiary = async () => {
+    if (confirm('삭제하시겠습니까?')) {
+      try {
+        const message = await api.deleteDiaryDetail(id as string);
+        alert(message);
+        // TODO: 일기 삭제 후 라우팅 처리 수정
+        router.back();
+      } catch (error) {
+        if (isAxiosError<ErrorResponse>(error)) {
+          alert(errorResponseMessage(error.response?.data.message));
+        }
+      }
+    }
+  };
 
   return (
     <Section>
@@ -36,9 +56,7 @@ const DiaryDetailPage: NextPageWithLayout = () => {
               {
                 icon: <TrashIcon />,
                 label: '삭제하기',
-                onClick: () => {
-                  alert('삭제하시겠습니까?');
-                },
+                onClick: handleDeleteDiary,
               },
             ]}
           />
