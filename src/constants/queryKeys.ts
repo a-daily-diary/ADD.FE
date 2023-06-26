f26ddf324a2389272af9fdc4848c9b0f690aaa2d
Bori-github <qhflrnfl4324@gmail.com>
2023-06-27 01:21:05 +0900

    ✨ #133 - useWriteComment mutation 커스텀 훅 생성

---
+++ src/constants/queryKeys.ts
@@ -0,0 +1,9 @@
+export const queryKeys = {
+  badges: 'badges',
+  bookmark: 'bookmark',
+  comment: 'comment',
+  diaries: 'diaries',
+  favorite: 'favorite',
+  terms: 'terms-agreements',
+  users: 'users',
+} as const;
---
+++ src/hooks/services/mutations/useWriteComment.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import type { CommentRequest } from 'types/Comment';
+import * as api from 'api';
+
+export const useWriteComment = (diaryId: string) => {
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(
+    async ({ diaryId, comment }: CommentRequest) =>
+      await api.writeComment({
+        diaryId,
+        comment,
+      }),
+    {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(['comments', diaryId]);
+      },
+    },
+  );
+
+  return mutate;
+};
