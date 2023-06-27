cfc149748861a08c322ca034ccf6e23f8dd2fd42
Bori-github <qhflrnfl4324@gmail.com>
2023-06-27 20:58:44 +0900

    ✨ #139 - 일기 북마크/북마크 취소 mutation 커스텀 훅 생성

--- src/hooks/services/index.ts
+++ src/hooks/services/index.ts
@@ -2,3 +2,5 @@ export * from './mutations/useDeleteComment';
 export * from './mutations/useWriteComment';
 export * from './mutations/useCancelFavoriteDiary';
 export * from './mutations/useFavoriteDiary';
+export * from './mutations/useCancelBookmarkDiary';
+export * from './mutations/useBookmarkDiary';
