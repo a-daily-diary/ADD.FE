fe9f2d3573b6b1cb928233733461ee64f495427e
Bori-github <qhflrnfl4324@gmail.com>
2023-07-18 15:53:43 +0900

    ✨ #149 - 사용자 프로필 데이터 보여주기
    - ProfileContainer 컴포넌트 생성 및 적용

---
+++ src/containers/profile/ProfileContainer.tsx
@@ -0,0 +1,65 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import Link from 'next/link';
+import { SettingIcon } from 'assets/icons';
+import { useProfile } from 'hooks/services';
+
+interface ProfileContainerProps {
+  username: string;
+}
+
+export const ProfileContainer = ({ username }: ProfileContainerProps) => {
+  const { profileData, isLoading } = useProfile(username);
+
+  if (profileData === undefined) return <div />;
+  if (isLoading) return <div>Loading</div>;
+
+  return (
+    <Container>
+      <SettingLink href={'/setting'}>
+        <SettingIcon />
+      </SettingLink>
+      <ProfileImage
+        src={profileData.imgUrl}
+        alt={profileData.username}
+        width={92}
+        height={92}
+        priority
+      />
+      <UserName>{username}</UserName>
+      <EditLink href={'/profile/edit'}>프로필 수정</EditLink>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  padding: 32px 20px;
+  border-bottom: 12px solid ${({ theme }) => theme.colors.gray_06};
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const SettingLink = styled(Link)`
+  position: absolute;
+  top: 32px;
+  right: 20px;
+`;
+
+const ProfileImage = styled(Image)`
+  border-radius: 50%;
+`;
+
+const UserName = styled.h2`
+  margin: 8px 0 6px;
+  ${({ theme }) => theme.fonts.headline_01};
+`;
+
+const EditLink = styled(Link)`
+  padding: 12px 20px;
+  border-radius: 120px;
+  background: #f4f4f4;
+  ${({ theme }) => theme.fonts.caption_01};
+`;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,11 +1,16 @@
 import styled from '@emotion/styled';
-import Link from 'next/link';
-import type { NextPage } from 'next';
-import { SettingIcon } from 'assets/icons';
+import { QueryClient, dehydrate } from '@tanstack/react-query';
+import { getServerSession } from 'next-auth';
+import { useSession } from 'next-auth/react';
+import type { GetServerSideProps, NextPage } from 'next';
+import * as api from 'api';
 import Seo from 'components/common/Seo';
 import Tab from 'components/common/Tab';
 import Empty from 'components/profile/Empty';
+import { queryKeys } from 'constants/queryKeys';
+import { ProfileContainer } from 'containers/profile/ProfileContainer';
 import { useTabIndicator } from 'hooks';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
 
 const PROFILE_TAB_LIST = [
   { id: 'activities', title: '활동' },
@@ -16,17 +21,14 @@ const PROFILE_TAB_LIST = [
 const Profile: NextPage = () => {
   const { tabsRef, indicator, activeIndex, setActiveIndex } = useTabIndicator();
 
+  const { data: session } = useSession();
+
+  if (session === null) return <div>로그인이 필요합니다.</div>; // TODO: 로그인 페이지로 이동 모달 생성하여 적용하기
+
   return (
     <>
       <Seo title="프로필 | a daily diary" />
-      <UserInfoContainer>
-        <SettingLink href={'/setting'}>
-          <SettingIcon />
-        </SettingLink>
-        <UserProfileImage />
-        <UserName>userid1234</UserName>
-        <ProfileEditLink href={'/profile/edit'}>프로필 수정</ProfileEditLink>
-      </UserInfoContainer>
+      <ProfileContainer username={session.user.username} />
       <section>
         <Tab indicator={indicator}>
           {PROFILE_TAB_LIST.map((tab, index) => {
@@ -66,42 +68,35 @@ const Profile: NextPage = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req, res } = context;
+  const session = await getServerSession(req, res, authOptions);
+
+  if (session === null) {
+    return {
+      redirect: {
+        destination: '/account/login',
+        permanent: false,
+      },
+    };
+  }
+
+  const { username, accessToken } = session.user;
+
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery([queryKeys.users, username], async () => {
+    return await api.getProfileByUsername({ username, config: headers });
+  });
+  return { props: { dehydratedState: dehydrate(queryClient), session } };
+};
+
 export default Profile;
-
-const UserInfoContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  padding: 32px 20px;
-  border-bottom: 12px solid ${({ theme }) => theme.colors.gray_06};
-  background-color: ${({ theme }) => theme.colors.white};
-`;
-
-const SettingLink = styled(Link)`
-  position: absolute;
-  top: 32px;
-  right: 20px;
-`;
-
-const UserProfileImage = styled.div`
-  width: 92px;
-  height: 92px;
-  border-radius: 50%;
-  background-color: #d9d9d9;
-`;
-
-const UserName = styled.h2`
-  margin: 8px 0 6px;
-  ${({ theme }) => theme.fonts.headline_01};
-`;
-
-const ProfileEditLink = styled(Link)`
-  padding: 12px 20px;
-  border-radius: 120px;
-  background: #f4f4f4;
-  ${({ theme }) => theme.fonts.caption_01};
-`;
 
 const TabButton = styled.button<{ active: boolean }>`
   ${({ theme }) => theme.fonts.headline_04};
