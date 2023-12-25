34eca2260b0cdaf395b44e300716106327173b23
Bori-github <qhflrnfl4324@gmail.com>
2023-12-25 21:21:48 +0900

    ⚡️ #162 - 뒤로가기 이벤트 발생 시 confirm 대신 원하는 동작이 이루어질 수 있도록 수정
    - 뒤로가기 이벤트 발생 시 모달이 나타날 수 있도록 코드 수정

--- src/hooks/common/useBeforeLeave.ts
+++ src/hooks/common/useBeforeLeave.ts
@@ -1,30 +1,48 @@
-import router from 'next/router';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 interface UseBeforeLeaveProps {
-  message: string;
-  path: string;
+  beforeLeaveCallback: () => void;
 }
 
-export const useBeforeLeave = ({ message, path }: UseBeforeLeaveProps) => {
-  const handleConfirm = () => {
-    window.history.pushState(null, '', path);
-    if (confirm(message)) return true;
-    return false;
-  };
+export const useBeforeLeave = ({
+  beforeLeaveCallback,
+}: UseBeforeLeaveProps) => {
+  const router = useRouter();
 
   const handleBeforeunload = (e: BeforeUnloadEvent) => {
     e.preventDefault();
     return (e.returnValue = '');
   };
 
+  const handleRouteChange = () => {
+    if (router.asPath !== window.location.pathname) {
+      window.history.pushState(null, '', router.asPath);
+    }
+
+    // NOTE: route가 변경되기 전 실행할 함수
+    beforeLeaveCallback();
+
+    // NOTE: 뒤로가기 이벤트 실행을 막기 위한 코드
+    router.events.emit('routeChangeError');
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw undefined;
+  };
+
+  const handleRouterBack = () => {
+    router.events.off('routeChangeStart', handleRouteChange);
+    router.back();
+  };
+
   useEffect(() => {
     window.addEventListener('beforeunload', handleBeforeunload);
-    router.beforePopState(() => handleConfirm());
+    router.events.on('routeChangeStart', handleRouteChange);
 
     return () => {
       window.removeEventListener('beforeunload', handleBeforeunload);
-      router.beforePopState(() => true);
+      router.events.off('routeChangeStart', handleRouteChange);
     };
-  }, [handleBeforeunload, handleConfirm]);
+  }, [handleBeforeunload, handleRouteChange]);
+
+  return { handleRouterBack };
 };
