62f9620b653c099bb42a27d9533879c1d1341f47
yws1502 <yws1502@naver.com>
2023-12-27 21:33:17 +0900

    💄 #160 - 랜덤 매칭 규칙 페이지 마크업, 스타일 완료

--- src/pages/matching/index.tsx
+++ src/pages/matching/index.tsx
@@ -3,8 +3,11 @@ import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 
+import { EnIcon, BlockIcon, PrivacyIcon, DiseaseIcon } from 'assets/icons';
+
 import { Button, Seo } from 'components/common';
 import { Header, HeaderLeft, HeaderTitle } from 'components/layouts';
+import { colors } from 'constants/styles';
 import { ScreenReaderOnly } from 'styles';
 
 const MatchingRule: NextPage = () => {
@@ -27,26 +30,48 @@ const MatchingRule: NextPage = () => {
           title={<HeaderTitle title="랜덤 매칭" />}
           right={<div />}
         />
-        <article>
-          <h2>즐거운 랜던매칭 통화를 위해 랜덤 매칭 규칙을 꼭 지켜주세요!</h2>
-          <ul>
-            {Array.from({ length: 4 }).map((_, idx) => {
-              const key = `loop-${idx}`;
-              return (
-                <li key={key}>
-                  <span>image</span>
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Vel, modi.
-                  </p>
-                </li>
-              );
-            })}
-          </ul>
+        <Article>
+          <h2>
+            즐거운 랜덤 매칭 통화를 위해
+            <br />
+            랜덤 매칭 규칙을 꼭 지켜주세요!
+          </h2>
+          <RuleList>
+            <li>
+              <ImageWrapper>
+                <EnIcon />
+              </ImageWrapper>
+              <p>한국어 보다는 영어 사용을 권장합니다.</p>
+            </li>
+            <li>
+              <ImageWrapper>
+                <BlockIcon />
+              </ImageWrapper>
+              <p>
+                특정 횟수의 경고를 받은 사용자는 일부 서비스 사용이 차단될 수
+                있습니다.
+              </p>
+            </li>
+            <li>
+              <ImageWrapper>
+                <PrivacyIcon />
+              </ImageWrapper>
+              <p>개인정보 노출 혹은 요청을 금지합니다.</p>
+            </li>
+            <li>
+              <ImageWrapper>
+                <DiseaseIcon />
+              </ImageWrapper>
+              <p>
+                선정적인 언어, 혐오/차별/폭력적인 언어, 불법 행위/기타 행위를
+                제한합니다.
+              </p>
+            </li>
+          </RuleList>
           <Button type="button" pattern="round" size="xl">
             랜덤매칭 시작
           </Button>
-        </article>
+        </Article>
       </Section>
     </>
   );
@@ -56,9 +81,49 @@ export default MatchingRule;
 
 const Section = styled.section`
   margin-top: 54px;
-  min-height: 100vh;
 `;
 
 const Title = styled.h1`
   ${ScreenReaderOnly}
+`;
+
+const Article = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 36px 38px 0;
+  h2 {
+    ${({ theme }) => theme.fonts.headline_02}
+    text-align: center;
+    margin-bottom: 26px;
+  }
+`;
+
+const RuleList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 50px;
+  li {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    p {
+      ${({ theme }) => theme.fonts.body_04}
+    }
+  }
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  background-color: ${colors.bg_02};
+  min-width: 60px;
+  height: 60px;
+  border-radius: 20px;
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 `;
