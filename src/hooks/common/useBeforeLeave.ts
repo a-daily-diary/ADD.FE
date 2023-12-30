dde3ae7dc3749d889b41e07c53731cc0daa5c7b2
Bori-github <qhflrnfl4324@gmail.com>
2023-12-30 19:36:59 +0900

    🐛 #162 - router 변경 감지 > 뒤로가기 이벤트 감지로 변경

--- src/hooks/common/useBeforeLeave.ts
+++ src/hooks/common/useBeforeLeave.ts
@@ -15,34 +15,34 @@ export const useBeforeLeave = ({
     return (e.returnValue = '');
   };
 
-  const handleRouteChange = () => {
-    if (router.asPath !== window.location.pathname) {
+  const handleBeforePopstate = (as: string) => {
+    if (router.asPath !== as) {
       window.history.pushState(null, '', router.asPath);
+
+      void router.push(router.asPath);
+
+      beforeLeaveCallback();
+
+      return false;
     }
 
-    // NOTE: route가 변경되기 전 실행할 함수
-    beforeLeaveCallback();
-
-    // NOTE: 뒤로가기 이벤트 실행을 막기 위한 코드
-    router.events.emit('routeChangeError');
-    // eslint-disable-next-line @typescript-eslint/no-throw-literal
-    throw undefined;
+    return true;
   };
 
   const handleRouterBack = () => {
-    router.events.off('routeChangeStart', handleRouteChange);
+    router.beforePopState(() => true);
     router.back();
   };
 
   useEffect(() => {
     window.addEventListener('beforeunload', handleBeforeunload);
-    router.events.on('routeChangeStart', handleRouteChange);
+    router.beforePopState(({ as }) => handleBeforePopstate(as));
 
     return () => {
       window.removeEventListener('beforeunload', handleBeforeunload);
-      router.events.off('routeChangeStart', handleRouteChange);
+      router.beforePopState(() => true);
     };
-  }, [handleBeforeunload, handleRouteChange]);
+  }, [handleBeforeunload, handleBeforePopstate]);
 
   return { handleRouterBack };
 };
