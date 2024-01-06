3ab90f4392c07a67ac266e8606e78e167e028299
yws1502 <yws1502@naver.com>
2024-01-06 23:29:14 +0900

    ♻️ #170 - 매칭 통화 컨트롤러 컴포넌트 분리

--- src/pages/matching/playing/index.tsx
+++ src/pages/matching/playing/index.tsx
@@ -3,11 +3,10 @@ import styled from '@emotion/styled';
 import React from 'react';
 import type { NextPage } from 'next';
 
-import { MicrophoneOffIcon, EndCallIcon } from 'assets/icons';
 import { Seo } from 'components/common';
+import MatchingController from 'components/matching/MatchingController';
 import MatchingUserInfo from 'components/matching/MatchingUserInfo';
 import RecommendTopic from 'components/matching/RecommendTopic';
-import { colors } from 'constants/styles';
 import { ScreenReaderOnly } from 'styles';
 
 const MatchingPlaying: NextPage = () => {
@@ -18,18 +17,7 @@ const MatchingPlaying: NextPage = () => {
         <Title>랜덤 매칭</Title>
         <MatchingUserInfo />
         <RecommendTopic />
-        <BottomArea className="bottom-area">
-          <SubTitle>통화 제어</SubTitle>
-          <CircleButton type="button" backgroundColor={colors.bg_02}>
-            <Tooltip>마이크를 켜주세요!</Tooltip>
-            <MicrophoneOffIcon />
-            <span>마이크 off</span>
-          </CircleButton>
-          <CircleButton type="button" backgroundColor={colors.red}>
-            <EndCallIcon />
-            <span>통화 종료</span>
-          </CircleButton>
-        </BottomArea>
+        <MatchingController />
       </Section>
     </>
   );
@@ -41,64 +29,7 @@ const Title = styled.h1`
   ${ScreenReaderOnly}
 `;
 
-const SubTitle = styled.h2`
-  ${ScreenReaderOnly}
-`;
-
 const Section = styled.section`
   text-align: center;
   padding: 0 20px;
-`;
-
-const BottomArea = styled.article`
-  display: flex;
-  justify-content: space-around;
-  margin: 53px 0 30px;
-`;
-
-const CircleButton = styled.button<{
-  backgroundColor: string;
-}>`
-  position: relative;
-  width: 60px;
-  height: 60px;
-  border-radius: 100%;
-  background-color: ${(props) => props.backgroundColor};
-  svg {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  span {
-    position: absolute;
-    bottom: -26px;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 60px;
-    color: ${({ theme }) => theme.colors.gray_00};
-    ${({ theme }) => theme.fonts.body_07}
-  }
-`;
-
-const Tooltip = styled.div`
-  ${({ theme }) => theme.fonts.button_02}
-  position: absolute;
-  top: -46px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 150px;
-  background-color: ${({ theme }) => theme.colors.primary_00};
-  color: ${({ theme }) => theme.colors.white};
-  border-radius: 6px;
-  padding: 10px 20px;
-  &::before {
-    content: '';
-    position: absolute;
-    bottom: -16px;
-    left: 50%;
-    transform: translateX(-50%);
-    border: 8px solid transparent;
-    border-top-color: ${({ theme }) => theme.colors.primary_00};
-  }
 `;
