163afcc3a45c0c213d26db2a7c64262aedb1b8ae
Bori-github <qhflrnfl4324@gmail.com>
2024-01-07 18:02:36 +0900

    💄 #172 - Rotation animation 생성

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,4 +3,5 @@ export { default as EllipsisStyle } from './EllipsisStyle';
 export { default as GlobalStyle } from './GlobalStyle';
 export { default as SVGVerticalAlignStyle } from './SVGVerticalAlignStyle';
 export { default as FadeInAnimationStyle } from './FadeInAnimationStyle';
+export { default as RotationAnimationStyle } from './RotationAnimationStyle';
 export * from './ScreenReaderStyle';
