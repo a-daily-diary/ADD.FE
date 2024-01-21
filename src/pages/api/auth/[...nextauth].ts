cad8ed06fa166b631c0e54fa5e5b31bff2dae2f5
Bori-github <qhflrnfl4324@gmail.com>
2024-01-21 14:20:26 +0900

    🐛 #175 - next-auth session maxAge 24시간으로 변경

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,7 +11,7 @@ import { errorResponseMessage } from 'utils';
 export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60, // 30 days
+    maxAge: 24 * 60 * 60, // NOTE: 24 hours
   },
   providers: [
     // email, password를 이용한 인증 방식
