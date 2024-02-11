f37347e234ae5fbec19170b3d0e864a33fc5e862
Bori-github <qhflrnfl4324@gmail.com>
2024-02-12 01:21:26 +0900

    ✨ #185 - NoLinkProfileImage 컴포넌트 생성 및 적용

--- src/components/profile/ProfileImage.tsx
+++ src/components/profile/ProfileImage.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -5,7 +6,7 @@ import Link from 'next/link';
 interface ProfileImageProps {
   username: string;
   src: string;
-  size: 'sm' | 'md';
+  size: 'sm' | 'md' | 'lg' | 'xl';
 }
 
 export const ProfileImage = ({ username, src, size }: ProfileImageProps) => {
@@ -21,13 +22,43 @@ export const ProfileImage = ({ username, src, size }: ProfileImageProps) => {
   );
 };
 
+export const NoLinkProfileImage = ({
+  username,
+  src,
+  size,
+}: ProfileImageProps) => {
+  return (
+    <StyledImage
+      src={src}
+      alt={username}
+      width={SIZE_STYLES[size]}
+      height={SIZE_STYLES[size]}
+      priority
+    />
+  );
+};
+
 const SIZE_STYLES = {
   sm: 22,
   md: 28,
+  lg: 92,
+  xl: 160,
 };
 
-const ImageLink = styled(Link)`
+const ImageStyles = css`
   overflow: hidden;
   border-radius: 50%;
   aspect-ratio: 1;
+  object-fit: cover;
+`;
+
+const ImageLink = styled(Link)`
+  ${ImageStyles}
+`;
+
+const StyledImage = styled(Image)`
+  ${ImageStyles}
+
+  display: block;
+  margin: 0 auto;
 `;
