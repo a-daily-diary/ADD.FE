d25b6b7410e9159cdea27aacf7c50310b54c93b6
Bori <85009583+Bori-github@users.noreply.github.com>
2024-02-12 16:29:55 +0900

    다른 사용자 프로필 페이지 구현  (#180)
    * ✨ #178 - 나의 프로필과 다른 사용자의 프로필을 구분하기 위한 props 추가
    
    * ✨ #178 - 다른 사용자 프로필 페이지 구현
    
    * ✏️ #178 - 나의 프로필 컴포넌트 명 수정
    
    * ♻️ #178 - DiariesContainer title props 추가
    
    * ♻️ #178 - DiariesContainer diariesData가 비어있는 경우 불필요한 프래그먼트 삭제
    
    * 🔥 #178 - 불필요한 타입 제거

--- src/components/diary/DiariesContainer.tsx
+++ src/components/diary/DiariesContainer.tsx
@@ -1,34 +1,42 @@
 import styled from '@emotion/styled';
 import Diary from './Diary';
-import type { ReactNode } from 'react';
 import type { Diaries } from 'types/diary';
+import { ScreenReaderOnly } from 'styles';
 
 interface DiariesContainerProps {
+  title: string;
   diariesData: Diaries;
-  empty?: ReactNode;
+  empty: JSX.Element;
 }
 
 export const DiariesContainer = ({
+  title,
   diariesData,
   empty,
 }: DiariesContainerProps) => {
   const { diaries } = diariesData;
+  const isEmptyDiaries = diaries === undefined || diaries.length === 0;
 
   // TODO: 북마크한 일기 리스트 조회 데이터 구조 변경 완료 후 수정
   // diaries의 값이 null일 경우가 있는지 확인
-  if (diaries === undefined || diaries.length === 0) {
-    return <>{empty}</>;
-  }
+  if (isEmptyDiaries) return empty;
 
   return (
-    <List>
-      {diaries.map((diary) => {
-        const { id } = diary;
-        return <Diary key={`diary-list-${id}`} {...diary} />;
-      })}
-    </List>
+    <article>
+      <Title>{title}</Title>
+      <List>
+        {diaries.map((diary) => {
+          const { id } = diary;
+          return <Diary key={`diary-list-${id}`} {...diary} />;
+        })}
+      </List>
+    </article>
   );
 };
+
+const Title = styled.h2`
+  ${ScreenReaderOnly}
+`;
 
 const List = styled.ul`
   display: grid;
