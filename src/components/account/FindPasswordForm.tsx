f90bb3db6fef5e77ce7ef2a432cd05be612bb289
sohyeonAn <ansh4393@gmail.com>
2024-02-12 18:51:35 +0900

    🎨 #183 - title, description, button 을 FindPasswordForm 컴포넌트로 이동

--- src/components/account/FindPasswordForm.tsx
+++ src/components/account/FindPasswordForm.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { useFormContext } from 'react-hook-form';
 import type { RegisterForm } from 'types/register';
+import { Button } from 'components/common';
 import { FormInput } from 'components/form';
 import { ERROR_MESSAGE, VALID_VALUE } from 'constants/validation';
 
@@ -12,23 +13,57 @@ export const FindPasswordForm = () => {
   const { register } = useFormContext<RegisterForm>();
 
   return (
-    <Form>
-      <FormInput
-        register={register('email', {
-          required: ERROR_MESSAGE.email.required,
-          pattern: {
-            value: VALID_VALUE.email,
-            message: ERROR_MESSAGE.email.pattern,
-          },
-        })}
-        type="text"
-        placeholder="이메일"
-        label="이메일"
-      />
-    </Form>
+    <>
+      <Title>비밀번호를 잊으셨나요?</Title>
+      <DescriptionText>
+        이메일 주소를 입력하면 암호를 재설정할 수 있는
+        <br />
+        링크를 이메일로 보내드릴게요.
+      </DescriptionText>
+      <Form>
+        <FormInput
+          register={register('email', {
+            required: ERROR_MESSAGE.email.required,
+            pattern: {
+              value: VALID_VALUE.email,
+              message: ERROR_MESSAGE.email.pattern,
+            },
+          })}
+          type="text"
+          placeholder="이메일"
+          label="이메일"
+        />
+        <ButtonContainer>
+          <Button
+            type="submit"
+            disabled={!isValid}
+            pattern="box"
+            size="lg"
+            fullWidth
+          >
+            재설정 링크보내기
+          </Button>
+        </ButtonContainer>
+      </Form>
+    </>
   );
 };
 
+const Title = styled.h1`
+  ${({ theme }) => theme.fonts.headline_01};
+`;
+
+const DescriptionText = styled.p`
+  margin-top: 8px;
+
+  color: ${({ theme }) => theme.colors.gray_02};
+  ${({ theme }) => theme.fonts.body_07};
+`;
+
 const Form = styled.form`
   margin-top: 40px;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 28px;
 `;
--- src/pages/account/findPassword.tsx
+++ src/pages/account/findPassword.tsx
@@ -3,7 +3,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import type { NextPage } from 'next/types';
 import type { RegisterForm } from 'types/register';
 import { FindPasswordForm } from 'components/account';
-import { Button, Seo } from 'components/common';
+import { Seo } from 'components/common';
 import { Header, HeaderLeft, HeaderTitle } from 'components/layouts';
 
 const FindPassword: NextPage = () => {
@@ -24,25 +24,8 @@ const FindPassword: NextPage = () => {
         title={<HeaderTitle title={'비밀번호 찾기'} position={'left'} />}
       />
       <ContentWrapper>
-        <Title>비밀번호를 잊으셨나요?</Title>
-        <DescriptionText>
-          이메일 주소를 입력하면 암호를 재설정할 수 있는
-          <br />
-          링크를 이메일로 보내드릴게요.
-        </DescriptionText>
         <FormProvider {...methods}>
           <FindPasswordForm />
-          <ButtonContainer>
-            <Button
-              type="submit"
-              disabled={!isValid}
-              pattern="box"
-              size="lg"
-              fullWidth
-            >
-              재설정 링크보내기
-            </Button>
-          </ButtonContainer>
         </FormProvider>
       </ContentWrapper>
     </>
@@ -54,19 +37,4 @@ export default FindPassword;
 const ContentWrapper = styled.section`
   margin-top: 54px;
   padding: 30px 20px;
-`;
-
-const Title = styled.h1`
-  ${({ theme }) => theme.fonts.headline_01};
-`;
-
-const DescriptionText = styled.p`
-  margin-top: 8px;
-
-  color: ${({ theme }) => theme.colors.gray_02};
-  ${({ theme }) => theme.fonts.body_07};
-`;
-
-const ButtonContainer = styled.div`
-  margin-top: 28px;
 `;
