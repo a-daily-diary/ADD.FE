d17b8ba0028e7409b6f79d74060c0a43375c844b
Bori-github <qhflrnfl4324@gmail.com>
2024-02-25 01:31:57 +0900

    ✨ #197 - 작은 크기의 Loading 컴포넌트 생성 및 기존 컴포넌트 명 수정
    - 기존 Loading > FullPageLoading
    - 새로 생성한 작은 컴포넌트 Loading

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,22 +1,38 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { LoadingIcon } from 'assets/icons';
 import { Z_INDEX } from 'constants/styles';
 import { RotationAnimationStyle, SVGVerticalAlignStyle } from 'styles';
 
+export const FullPageLoading = () => {
+  return (
+    <FullPageContainer>
+      <IconContainer>
+        <LoadingIcon width={136} height={136} />
+      </IconContainer>
+    </FullPageContainer>
+  );
+};
+
 export const Loading = () => {
   return (
     <Container>
       <IconContainer>
-        <LoadingIcon />
+        <LoadingIcon width={32} height={32} />
       </IconContainer>
     </Container>
   );
 };
 
-const Container = styled.div`
+const ContainerStyle = css`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+const FullPageContainer = styled.div`
+  ${ContainerStyle}
+
   position: fixed;
   top: 0;
   right: 0;
@@ -24,6 +40,13 @@ const Container = styled.div`
   left: 0;
   z-index: ${Z_INDEX.modal};
   background-color: ${({ theme }) => theme.colors.black_overlay_06};
+`;
+
+const Container = styled.div`
+  ${ContainerStyle}
+
+  padding: 20px 0;
+  background-color: ${({ theme }) => theme.colors.white};
 `;
 
 const IconContainer = styled.div`
