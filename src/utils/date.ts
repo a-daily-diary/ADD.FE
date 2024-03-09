bd72f2870fa8db4124a46ee35fc4e253b350d110
Bori-github <qhflrnfl4324@gmail.com>
2024-03-09 14:54:37 +0900

    🏷️ #59 - getLastYearDate 리턴 타입 명시

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,4 @@
-export const getLastYearDate = (date: Date) => {
+export const getLastYearDate = (date: Date): Date => {
   const lastYear = date.getFullYear() - 1;
   const monthInLastYear = date.getMonth();
   const dayInLastYear = date.getDate() + 1;
