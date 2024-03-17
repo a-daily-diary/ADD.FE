fd97959c027ce2653ed88f5b52528ee77c9e4128
Bori-github <qhflrnfl4324@gmail.com>
2024-03-17 14:19:56 +0900

    ✨ #208 - 연도별 활동 데이터를 조회를 위한 getYearsForActivitiesCalendar 유틸 함수 생성

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -5,3 +5,19 @@ export const getLastYearDate = (date: Date): Date => {
 
   return new Date(lastYear, monthInLastYear, dayInLastYear);
 };
+
+/**
+ * 활동 탭 캘린더를 위한 가입년도부터 현재년도까지 목록
+ * @params  registerYear 사용자 가입년도 string
+ * @returns string[] | [가입년도, ..., 현재년도]
+ */
+export const getYearsForActivitiesCalendar = (): string[] => {
+  const registerYear = 2023; // TODO: 사용자 가입연도로 수정
+  const nowYear = new Date().getFullYear();
+
+  const years = Array.from({ length: nowYear - registerYear + 1 }, (_, index) =>
+    String(nowYear - index),
+  );
+
+  return years;
+};
