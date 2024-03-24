a715a3dcd5ffd932388a08bd18bef7e85aa6bb7a
sohyeonAn <ansh4393@gmail.com>
2024-03-24 22:28:00 +0900

    ✨#221 - 비밀번호 재설정 페이지 추가

---
+++ src/pages/account/resetPassword.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import type { NextPage } from 'next/types';
+
+const ResetPassword: NextPage = () => {
+  return (
+    <>
+      <ContentWrapper />
+    </>
+  );
+};
+
+export default ResetPassword;
+
+const ContentWrapper = styled.section`
+  margin-top: 54px;
+  padding: 30px 20px;
+`;
