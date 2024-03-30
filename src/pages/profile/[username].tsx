bc4908c9bc3a4621bc1609a382da28f1afb14fff
Bori <85009583+Bori-github@users.noreply.github.com>
2024-03-30 17:47:07 +0900

    다른 사용자 프로필 내 활동 탭 구현 (#213)
    * 🩹 #212 - Loading > FullPageLoading로 로딩 컴포넌트 변경
    
    * ✨ #212 - 다른 사용자 프로필 활동 탭 구현
    
    * 🐛 #212 - 다른 사용자 프로필 페이지에서 Navbar 나타나게 수정
    
    * 💚 - 불필요한 import 제거하여 build 에러 해결

--- src/pages/profile/[username].tsx
+++ src/pages/profile/[username].tsx
@@ -8,10 +8,10 @@ import type {
   NextPage,
 } from 'next';
 import * as api from 'api';
-import { Loading, ObserverTarget, Seo, Tab } from 'components/common';
+import { FullPageLoading, ObserverTarget, Seo, Tab } from 'components/common';
 import { DiariesContainer } from 'components/diary';
 import EmptyDiary from 'components/diary/EmptyDiary';
-import { ProfileContainer } from 'components/profile';
+import { ActivitiesContainer, ProfileContainer } from 'components/profile';
 import { PAGE_PATH } from 'constants/common';
 import { queryKeys } from 'constants/services';
 import { useIntersectionObserver, useTabIndicator } from 'hooks/common';
@@ -34,7 +34,7 @@ const YourProfile: NextPage<
     onIntersect: fetchNextPage,
   });
 
-  if (userDiariesData === undefined) return <Loading />;
+  if (userDiariesData === undefined) return <FullPageLoading />;
 
   return (
     <>
@@ -59,6 +59,12 @@ const YourProfile: NextPage<
           );
         })}
       </Tab>
+      {PROFILE_TAB_LIST[activeIndex].id === 'activities' && (
+        <ActivitiesContainer
+          title={PROFILE_TAB_LIST[activeIndex].title}
+          username={username}
+        />
+      )}
       {PROFILE_TAB_LIST[activeIndex].id === 'diaries' && (
         <>
           <DiariesContainer
