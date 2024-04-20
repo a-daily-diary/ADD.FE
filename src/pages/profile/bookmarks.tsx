b75c8c36ffcdb783da90f63fdf02b0346eb2d63d
Bori-github <qhflrnfl4324@gmail.com>
2024-04-20 20:43:14 +0900

    ♻️ #232 - 프로필 > 활동, 일기, 북마크 페이지 컴포넌트로 분리

--- src/pages/profile/bookmarks.tsx
+++ src/pages/profile/bookmarks.tsx
@@ -3,30 +3,52 @@ import { getServerSession } from 'next-auth';
 import { useSession } from 'next-auth/react';
 import type { GetServerSideProps, NextPage } from 'next';
 import * as api from 'api';
-import { Seo } from 'components/common';
-import {
-  ProfileContainer,
-  ActivitiesContainer,
-  ProfileTab,
-} from 'components/profile';
+import { FullPageLoading, ObserverTarget, Seo } from 'components/common';
+import { DiariesContainer } from 'components/diary';
+import EmptyDiary from 'components/diary/EmptyDiary';
+import { ProfileContainer, ProfileTab } from 'components/profile';
 import { PAGE_PATH } from 'constants/common';
 import { MY_PROFILE_TAB_LIST } from 'constants/profile';
 import { queryKeys } from 'constants/services';
+import { useIntersectionObserver } from 'hooks/common';
+import { useBookmarkedDiaries } from 'hooks/services';
 import { authOptions } from 'pages/api/auth/[...nextauth]';
 
-const MyProfile: NextPage = () => {
+const MyProfileBookmarks: NextPage = () => {
   const { data: session } = useSession();
 
-  if (session === null) return <div>로그인이 필요합니다.</div>; // TODO:
+  if (session === null) return <div>로그인이 필요합니다.</div>; // TODO: 로그인 페이지로 이동 모달 생성하여 적용하기
+
+  const {
+    bookmarkedDiariesData,
+    isLoading: isBookmarkedDiariesLoading,
+    isError: isBookmarkedDiariesError,
+    fetchNextPage: fetchBookmarkedDiariesNextPage,
+  } = useBookmarkedDiaries(session.user.username);
+  const { setTargetRef: setBookmarkedDiariesTargetRef } =
+    useIntersectionObserver({
+      onIntersect: fetchBookmarkedDiariesNextPage,
+    });
+
+  if (bookmarkedDiariesData === undefined) {
+    return <FullPageLoading />;
+  }
+
   return (
     <>
       <Seo title="프로필 | a daily diary" />
       <ProfileContainer username={session.user.username} />
 
       <ProfileTab tabList={MY_PROFILE_TAB_LIST} />
-      <ActivitiesContainer
-        title="프로필 - 활동"
-        username={session.user.username}
+      <DiariesContainer
+        title="프로필 - 북마크"
+        diariesData={bookmarkedDiariesData}
+        empty={<EmptyDiary text="북마크한 일기가 없습니다." />}
+      />
+      <ObserverTarget
+        targetRef={setBookmarkedDiariesTargetRef}
+        isLoading={isBookmarkedDiariesLoading}
+        isError={isBookmarkedDiariesError}
       />
     </>
   );
@@ -57,10 +79,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   await queryClient.prefetchQuery([queryKeys.users, username], async () => {
     return await api.getProfileByUsername({ username, config: headers });
   });
-  await queryClient.prefetchQuery([queryKeys.badges, username], async () => {
-    return await api.getBadgesByUsername({ username, config: headers });
-  });
   return { props: { dehydratedState: dehydrate(queryClient), session } };
 };
 
-export default MyProfile;
+export default MyProfileBookmarks;
--- src/pages/profile/diaries.tsx
+++ src/pages/profile/diaries.tsx
@@ -3,30 +3,51 @@ import { getServerSession } from 'next-auth';
 import { useSession } from 'next-auth/react';
 import type { GetServerSideProps, NextPage } from 'next';
 import * as api from 'api';
-import { Seo } from 'components/common';
-import {
-  ProfileContainer,
-  ActivitiesContainer,
-  ProfileTab,
-} from 'components/profile';
+import { FullPageLoading, ObserverTarget, Seo } from 'components/common';
+import { DiariesContainer } from 'components/diary';
+import EmptyDiary from 'components/diary/EmptyDiary';
+import { ProfileContainer, ProfileTab } from 'components/profile';
 import { PAGE_PATH } from 'constants/common';
 import { MY_PROFILE_TAB_LIST } from 'constants/profile';
 import { queryKeys } from 'constants/services';
+import { useIntersectionObserver } from 'hooks/common';
+import { useUserDiaries } from 'hooks/services';
 import { authOptions } from 'pages/api/auth/[...nextauth]';
 
-const MyProfile: NextPage = () => {
+const MyProfileDiaries: NextPage = () => {
   const { data: session } = useSession();
 
-  if (session === null) return <div>로그인이 필요합니다.</div>; // TODO:
+  if (session === null) return <div>로그인이 필요합니다.</div>; // TODO: 로그인 페이지로 이동 모달 생성하여 적용하기
+
+  const {
+    userDiariesData,
+    isLoading: isUserDiariesLoading,
+    isError: isUserDiariesError,
+    fetchNextPage: fetchUserDiariesNextPage,
+  } = useUserDiaries(session.user.username);
+  const { setTargetRef: setUserDiariesTargetRef } = useIntersectionObserver({
+    onIntersect: fetchUserDiariesNextPage,
+  });
+
+  if (userDiariesData === undefined) {
+    return <FullPageLoading />;
+  }
+
   return (
     <>
       <Seo title="프로필 | a daily diary" />
       <ProfileContainer username={session.user.username} />
 
       <ProfileTab tabList={MY_PROFILE_TAB_LIST} />
-      <ActivitiesContainer
-        title="프로필 - 활동"
-        username={session.user.username}
+      <DiariesContainer
+        title="프로필 - 일기"
+        diariesData={userDiariesData}
+        empty={<EmptyDiary text="일기가 없습니다." />}
+      />
+      <ObserverTarget
+        targetRef={setUserDiariesTargetRef}
+        isLoading={isUserDiariesLoading}
+        isError={isUserDiariesError}
       />
     </>
   );
@@ -57,10 +78,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   await queryClient.prefetchQuery([queryKeys.users, username], async () => {
     return await api.getProfileByUsername({ username, config: headers });
   });
-  await queryClient.prefetchQuery([queryKeys.badges, username], async () => {
-    return await api.getBadgesByUsername({ username, config: headers });
-  });
   return { props: { dehydratedState: dehydrate(queryClient), session } };
 };
 
-export default MyProfile;
+export default MyProfileDiaries;
