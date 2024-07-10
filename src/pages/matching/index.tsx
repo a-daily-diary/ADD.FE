1482fb6684e82cc05217d40651e6d0f50131290d
yws1502 <yws1502@naver.com>
2024-07-10 21:52:03 +0900

    🩹 #217 - 매칭 페이지 마크업 수정

--- src/pages/matching/index.tsx
+++ src/pages/matching/index.tsx
@@ -9,7 +9,6 @@ import { Button, Seo } from 'components/common';
 import { Header, HeaderLeft, HeaderTitle } from 'components/layouts';
 import { PAGE_PATH } from 'constants/common';
 import { colors } from 'constants/styles';
-import { ScreenReaderOnly } from 'styles';
 
 const MatchingRule: NextPage = () => {
   const router = useRouter();
@@ -43,36 +42,33 @@ const MatchingRule: NextPage = () => {
   return (
     <>
       <Seo title="랜덤 매칭 규칙 | a daily diary" />
+      <Header
+        left={<HeaderLeft type="이전" />} // FIXME: 이전 버튼에 대해 클릭 이벤트 함수 props로 받을 수 있도록 셋팅
+        title={<HeaderTitle title="랜덤 매칭" position="center" />}
+      />
       <Section>
-        <Title>랜덤 매칭 규칙</Title>
-        <Header
-          left={<HeaderLeft type="이전" />} // FIXME: 이전 버튼에 대해 클릭 이벤트 함수 props로 받을 수 있도록 셋팅
-          title={<HeaderTitle title="랜덤 매칭" position="center" />}
+        <Title>
+          즐거운 랜덤 매칭 통화를 위해
+          <br />
+          랜덤 매칭 규칙을 꼭 지켜주세요!
+        </Title>
+        <RuleList>
+          {RULE_LIST.map((item) => {
+            return (
+              <RuleItem key={`rule-item-${item.id}`}>
+                <ImageWrapper>{item.icon}</ImageWrapper>
+                <p>{item.text}</p>
+              </RuleItem>
+            );
+          })}
+        </RuleList>
+        <Button
+          type="button"
+          shape="round"
+          size="xl"
+          onClick={handleGoToMatchingQueue}
+          text="랜덤매칭 시작"
         />
-        <Article>
-          <h2>
-            즐거운 랜덤 매칭 통화를 위해
-            <br />
-            랜덤 매칭 규칙을 꼭 지켜주세요!
-          </h2>
-          <RuleList>
-            {RULE_LIST.map((item) => {
-              return (
-                <RuleItem key={`rule-item-${item.id}`}>
-                  <ImageWrapper>{item.icon}</ImageWrapper>
-                  <p>{item.text}</p>
-                </RuleItem>
-              );
-            })}
-          </RuleList>
-          <Button
-            type="button"
-            shape="round"
-            size="xl"
-            onClick={handleGoToMatchingQueue}
-            text="랜덤매칭 시작"
-          />
-        </Article>
       </Section>
     </>
   );
@@ -81,23 +77,17 @@ const MatchingRule: NextPage = () => {
 export default MatchingRule;
 
 const Section = styled.section`
-  margin-top: 54px;
-`;
-
-const Title = styled.h1`
-  ${ScreenReaderOnly}
-`;
-
-const Article = styled.article`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 36px 38px 0;
-  h2 {
-    ${({ theme }) => theme.fonts.headline_02}
-    text-align: center;
-    margin-bottom: 26px;
-  }
+  margin-top: 54px;
+`;
+
+const Title = styled.h1`
+  ${({ theme }) => theme.fonts.headline_02}
+  text-align: center;
+  margin-bottom: 26px;
 `;
 
 const RuleList = styled.ul`
