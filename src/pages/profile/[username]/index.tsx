1b25decf05fc37c23ea5cd4298dd2eaddee362ca
Bori-github <qhflrnfl4324@gmail.com>
2024-08-11 17:38:51 +0900

    ♻️ #261 - 다른 사용자 프로필 페이지 getServerSidePropsWithAuth 적용

--- src/pages/profile/[username]/index.tsx
+++ src/pages/profile/[username]/index.tsx
@@ -1,21 +1,14 @@
 import { QueryClient, dehydrate } from '@tanstack/react-query';
 import { isAxiosError } from 'axios';
-import { getServerSession } from 'next-auth';
-import type {
-  GetServerSideProps,
-  InferGetServerSidePropsType,
-  NextPage,
-} from 'next';
+import type { GetServerSidePropsContext, NextPage } from 'next';
+import type { User } from 'next-auth';
 import * as api from 'api';
 import { ActivitiesContainer, ProfileLayout } from 'components/profile';
 import { PAGE_PATH } from 'constants/common';
-import { SERVER_SIDE_PROPS } from 'constants/server';
 import { queryKeys } from 'constants/services';
-import { authOptions } from 'pages/api/auth/[...nextauth]';
+import { getServerSidePropsWithAuth } from 'lib/auth';
 
-const YourProfile: NextPage<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ username }) => {
+const YourProfile: NextPage<{ username: string }> = ({ username }) => {
   return (
     <ProfileLayout isMyProfile={false} username={username}>
       <ActivitiesContainer title={`${username} - 활동`} username={username} />
@@ -23,55 +16,51 @@ const YourProfile: NextPage<
   );
 };
 
-export const getServerSideProps = (async (context) => {
-  const { req, res, params } = context;
-  const username = params?.username as string;
+export const getServerSideProps = getServerSidePropsWithAuth(
+  async (context: GetServerSidePropsContext) => {
+    const { user, query } = context;
+    const username = query?.username as string;
 
-  const session = await getServerSession(req, res, authOptions);
+    const { accessToken, username: loggedInUsername } = user as User;
 
-  if (session === null) {
-    return SERVER_SIDE_PROPS.REDIRECT_LOGIN;
-  }
-
-  const { accessToken, username: loggedInUsername } = session.user;
-
-  if (loggedInUsername === username) {
-    return {
-      redirect: {
-        destination: PAGE_PATH.profile.index,
-        permanent: false,
-      },
-    };
-  }
-
-  const headers = {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  };
-
-  const queryClient = new QueryClient();
-
-  try {
-    await queryClient.fetchQuery([queryKeys.users, username], async () => {
-      return await api.getProfileByUsername({ username, config: headers });
-    });
-    await queryClient.fetchQuery([queryKeys.badges, username], async () => {
-      return await api.getBadgesByUsername({
-        username,
-        onlyPinned: true,
-        config: headers,
-      });
-    });
-  } catch (error) {
-    if (isAxiosError(error)) {
+    if (loggedInUsername === username) {
       return {
-        notFound: true,
+        redirect: {
+          destination: PAGE_PATH.profile.index,
+          permanent: false,
+        },
       };
     }
-  }
 
-  return { props: { dehydratedState: dehydrate(queryClient), username } };
-}) satisfies GetServerSideProps;
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    const queryClient = new QueryClient();
+
+    try {
+      await queryClient.fetchQuery([queryKeys.users, username], async () => {
+        return await api.getProfileByUsername({ username, config: headers });
+      });
+      await queryClient.fetchQuery([queryKeys.badges, username], async () => {
+        return await api.getBadgesByUsername({
+          username,
+          onlyPinned: true,
+          config: headers,
+        });
+      });
+    } catch (error) {
+      if (isAxiosError(error)) {
+        return {
+          notFound: true,
+        };
+      }
+    }
+
+    return { props: { dehydratedState: dehydrate(queryClient), username } };
+  },
+);
 
 export default YourProfile;
