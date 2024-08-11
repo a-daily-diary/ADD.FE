730fd1a6f15a1f7c9f994dec93c3ed98519f3284
Bori-github <qhflrnfl4324@gmail.com>
2024-08-11 18:02:33 +0900

    🏷️ #261 - getServerSidePropsWithAuth 타입 수정
    - Promise 결과만 반환하도록 되어있던 타입을 Promise가 아닌 값도 반환할 수 있도록 수정

--- src/lib/auth/serverSidePropsWithAuth.ts
+++ src/lib/auth/serverSidePropsWithAuth.ts
@@ -1,10 +1,15 @@
 import { getServerSession } from 'next-auth';
-import type { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import type { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import type { GetServerSidePropsContext as GetServerSidePropsContextType } from 'next/types';
 import { SERVER_SIDE_PROPS } from 'constants/server';
 import { authOptions } from 'pages/api/auth/[...nextauth]';
 
+type WithAuthOptions<P> = (
+  context: GetServerSidePropsContextType,
+) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>;
+
 export const getServerSidePropsWithAuth =
-  (getServerSideProps: GetServerSideProps) =>
+  <P extends Record<string, unknown>>(getServerSideProps: WithAuthOptions<P>) =>
   async (context: GetServerSidePropsContext) => {
     const { req, res } = context;
     const session = await getServerSession(req, res, authOptions);
