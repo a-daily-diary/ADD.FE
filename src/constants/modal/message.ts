15dbdbabddbc3881ad04dd2981fad45f9fb0b3b9
Woosang <77317312+yws1502@users.noreply.github.com>
2024-08-19 21:09:53 +0900

    매칭 종료 이벤트 추가 (#266)
    * ➕ #260 - RTC 타입 관련 라이브러리 설치
    
    * ✨ #260 - 상대방이 연결을 종료한 경우 이벤트 처리
    
    * 🔥 #260 - 불필요한 null check 로직 제거
    
    * ✨ #260 - 매칭 종료 버튼 시 confirm 창 추가

--- src/constants/modal/message.ts
+++ src/constants/modal/message.ts
@@ -3,10 +3,12 @@ export const MODAL_MESSAGE = {
     '등록하지 않고 나갈 시 작성한 일기가 모두 삭제됩니다. 나가시겠습니까?',
   delete: '삭제하시겠습니까?',
   leaveMatching: '페이지 이동 시 매칭이 종료될 수 있습니다. 이동하시겠습니까?',
+  endMatching: '매칭을 종료하시겠습니까?',
 };
 
 export const MODAL_BUTTON = {
   confirm: '확인',
   delete: '삭제',
   leave: '나가기',
+  end: '종료',
 };
