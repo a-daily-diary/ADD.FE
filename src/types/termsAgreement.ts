6c57511a04a5e8cd5dfbf6231b55bf0eb91fedbb
yws1502 <yws1502@naver.com>
2025-02-11 22:24:27 +0900

    🏷️ #289 - 약관 동의 조회 데이터 구조 변경으로 인한 타입 수정

--- src/types/termsAgreement.ts
+++ src/types/termsAgreement.ts
@@ -1,8 +1,13 @@
 export type TermsAgreementId = 'service' | 'privacy';
 
+export interface TermsContent {
+  subTitle: string;
+  content: string;
+}
+
 export interface TermsAgreement {
   id: TermsAgreementId;
   title: string;
-  content: string;
+  contents: TermsContent[];
   isRequired: boolean;
 }
