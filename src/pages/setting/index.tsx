a3b42b44bcb0c78901301ec8e6b7d06c57b3ea93
sohyeonAn <ansh4393@gmail.com>
2025-02-23 23:09:12 +0900

    ✨ #269 - SettingMenus 컴포넌트 추가

--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -1,6 +1,8 @@
+import styled from '@emotion/styled';
 import type { NextPage } from 'next/types';
 import { Seo } from 'components/common';
 import { Header, HeaderLeft, HeaderTitle } from 'components/layouts';
+import { SettingMenus } from 'components/setting';
 
 const SettingPage: NextPage = () => {
   return (
@@ -10,8 +12,15 @@ const SettingPage: NextPage = () => {
         left={<HeaderLeft type="이전" />}
         title={<HeaderTitle title="설정" position="center" />}
       />
+      <Section>
+        <SettingMenus />
+      </Section>
     </>
   );
 };
 
 export default SettingPage;
+
+const Section = styled.section`
+  margin-top: 54px;
+`;
